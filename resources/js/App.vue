5248ae868216fd2998e65a0c9af66ca7d620aa6d
dkdom <dom.sayoudom@gmail.com>
2023-06-27 00:03:47 +0700

    Upload to GitHub

---
+++ resources/js/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h1>Hello myApp VueJS3</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MiniposApp',
+
+    data() {
+        return {
+            
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
